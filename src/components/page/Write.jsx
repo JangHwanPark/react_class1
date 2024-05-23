a5b6ad04b2c760fc267eba8a2f18460451801f92
JangHwanPark <wkd5878@gmail.com>
2024-05-23 19:23:39 +0900

    feat: 글쓰기 페이지 디자인 및 클릭이벤트 등록

--- src/components/page/Write.jsx
+++ src/components/page/Write.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import PageLayout from "./PageLayout";
+import {useNavigate} from "react-router-dom";
 
 export default function Write() {
+    const navigate = useNavigate();
+    const date = new Date();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        alert("서버가 존재하지 않습니다.");
+    }
+
+    const handleClickNavigate = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
+
     return (
-        <div>
-            <form action="">
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-                <button>글쓰기</button>
-            </form>
-        </div>
+        <PageLayout>
+            <div className="write-wrap">
+                <form action="" className="form-write">
+                    <input
+                        type="text"
+                        className="input-title"
+                        placeholder="제목을 입력하세요."
+                    />
+                    <div className="write-date">
+                        {date.getFullYear()}.{date.getMonth() + 1}.{date.getDate()}
+                    </div>
+                    <textarea
+                        className="text-body"
+                        placeholder="본문을 작성해주세요."
+                    />
+                    <div className="button-wrap flex justify-between">
+                        <button onClick={handleClickNavigate}>돌아가기</button>
+                        <div className="button-submit">
+                            <button onClick={handleClick}>임시저장</button>
+                            <button onClick={handleClick}>게시하기</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </PageLayout>
     );
 }
